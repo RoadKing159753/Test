8dc53c2cfc2fd890ccb153579f674ef7eff10bfb
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-08-23 16:36:11 +0530

    Update S3TestFile.tf

--- S3TestFile.tf
+++ S3TestFile.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "app_server12" {
   acl    = "private"
   versioning {
     enabled = true
-    
+    mfa_delete = true
   }
   logging {
     target_bucket = "pavantestbucket"
