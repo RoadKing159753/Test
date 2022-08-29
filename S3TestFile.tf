4146026020002eeb6782a95e5dcde9054f6ae3b2
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-08-29 15:08:58 +0530

    Update S3TestFile.tf

--- S3TestFile.tf
+++ S3TestFile.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
  #text
 
-resource "aws_s3_bucket" "app_server12" {
+resource "aws_s3_bucket" "unrestricted" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
