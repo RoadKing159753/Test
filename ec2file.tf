1a343b0e0104f4da21b3527df0fe11449717bf0d
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-10-11 20:14:11 +0530

    Update ec2file.tf

--- ec2file.tf
+++ ec2file.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  ingress_cidr_blocks = ["0.0.0.0/16"]
   tags = {
     Name = "ExampleAppServerInstance"
   }
